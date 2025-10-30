b746d3999b12c8c38cb5f37edc1d9e04e9c08c09
Spencer Boggs <spencerdboggs@gmail.com>
2025-10-30 13:29:44 -0700

    Created Subpages

--- app.py
+++ app.py
@@ -16,6 +16,38 @@ def create_app() -> Flask:
     def home():
         return render_template("home.html")
 
+    @app.route("/profile")
+    def profile():
+        return render_template("profile.html")
+
+    @app.route("/events")
+    def events():
+        return render_template("events.html")
+
+    @app.route("/settings")
+    def settings():
+        return render_template("settings.html")
+
+    @app.route("/weekly-insights")
+    def weekly_insights():
+        return render_template("weekly_insights.html")
+
+    @app.route("/matchmaking")
+    def matchmaking():
+        return render_template("matchmaking.html")
+
+    @app.route("/message")
+    def message():
+        return render_template("message.html")
+
+    @app.route("/blocked-accounts")
+    def blocked_accounts():
+        return render_template("blocked_accounts.html")
+
+    @app.route("/report")
+    def report():
+        return render_template("report.html")
+
     return app
 
 app = create_app()
